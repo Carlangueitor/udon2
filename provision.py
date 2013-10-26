8982a91bacc242e3c7d864a1cf66b995b649d46e
Charly Román <chack14rock@gmail.com>
2013-10-26 01:50:24 -0500

    Added Pillow instalation.

--- provision.py
+++ provision.py
@@ -31,6 +31,13 @@ def _setup_python():
     print blue("Setup Python")
     cuisine.package_ensure('python')
     cuisine.package_ensure('python-setuptools')
+    cuisine.package_ensure('python2.7-dev')
+    cuisine.package_ensure('libjpeg-dev')
+    cuisine.package_ensure('libjpeg62')
+    cuisine.package_ensure('libjpeg62-dev')
+    cuisine.package_ensure('zlib1g-dev')
+    cuisine.package_ensure('libfreetype6')
+    cuisine.package_ensure('libfreetype6-dev')
     sudo('easy_install pip', quiet=True)
     print blue("Installing python requirements.")
     sudo('pip install -r /home/vagrant/src/requirements/development.txt',
