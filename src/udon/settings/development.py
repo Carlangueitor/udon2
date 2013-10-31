5d81326795e59ed1f1150649f6e787b615815050
Charly Román <chack14rock@gmail.com>
2013-10-31 01:04:47 -0600

    Cleaned Settings

--- src/udon/settings/development.py
+++ src/udon/settings/development.py
@@ -1,5 +1,8 @@
 from .base import *  # NOQA
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
